fdc62200661d80ff8d39cdbf505bb707217178c4
alex26-dev <alexandruradoiu26@gmail.com>
2024-09-23 00:31:06 +0300

    Small Update
    Added range.clip in PController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDControllerAuto/PController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDControllerAuto/PController.java
@@ -10,6 +10,6 @@ public class PController {
     public double calculatePower(double currentPosition, double targetPosition) {
         double error = targetPosition - currentPosition;
 
-        return error * Kp;
+        return Range.clip(error * Kp, 1, -1);
     }
 }
